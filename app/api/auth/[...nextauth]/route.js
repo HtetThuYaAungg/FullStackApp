070fd69ed23d6af42d763fb203376c138789a8bd
Htet Thura Aung <htetthuraaung@abank.com.mm>
2023-06-30 14:31:38 +0630

    [fixed]

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import User from "@/app/models/User";
 import connect from "@/utils/db";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/Google";
+import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 
